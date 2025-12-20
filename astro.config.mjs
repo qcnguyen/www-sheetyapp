8e6edb05f26565ef34ce42b9e99b650acff3f9cc
qcnguyen <quocnt89@gmail.com>
2025-12-20 23:20:37 +0700

    improve SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,16 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
     site: 'https://sheetyapp.com',
+    integrations: [
+        sitemap({
+            filter: (page) => !page.includes('/404'),
+            changefreq: 'weekly',
+            priority: 0.7,
+            lastmod: new Date(),
+        })
+    ],
 });
