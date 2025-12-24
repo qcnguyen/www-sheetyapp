dc429ffff86095f942c2a1008068d42cbd6a741a
qcnguyen <quocnt89@gmail.com>
2025-12-24 23:46:18 +0700

    Documentation (#1)
    * w
    
    * w
    
    * w
    
    * w
    
    * w

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,64 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
+import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
     site: 'https://sheetyapp.com',
     integrations: [
+        starlight({
+            title: 'Sheety Documentation',
+            description: 'Official documentation for Sheety',
+            logo: {
+                src: './public/logo-64.png',
+                replacesTitle: false,
+            },
+            components: {
+                SiteTitle: './src/components/DocsHeader.astro',
+            },
+            customCss: [
+                // Relative path to your custom CSS file
+            ],
+            social: [
+            ],
+            sidebar: [
+                {
+                    label: 'Getting Started',
+                    items: [
+                        { label: 'Welcome', link: '/docs/' },
+                        { label: 'Quick Start', link: '/docs/getting-started' },
+                    ],
+                },
+                {
+                    label: 'Tutorials',
+                    items: [
+                        { 
+                            label: 'Workbooks & Sheets',
+                            collapsed: false,
+                            items: [
+                                { label: 'Creating Your First Workbook', link: '/docs/tutorials/creating-your-first-workbook' },
+                                { label: 'Adding Columns to Your Sheet', link: '/docs/tutorials/adding-columns' },
+                                { label: 'Adding Data to Your Sheet', link: '/docs/tutorials/adding-data' },
+                            ]
+                        },
+                    ],
+                },
+                {
+                    label: 'Concepts',
+                    collapsed: true,
+                    autogenerate: { directory: 'concepts' },
+                },
+                {
+                    label: 'Reference',
+                    collapsed: true,
+                    items: [
+                        { label: 'Q Expressions', link: '/docs/q-expressions' },
+                        { label: 'Row Actions', link: '/docs/row-actions' },
+                    ],
+                },
+            ],
+        }),
         sitemap({
             filter: (page) => !page.includes('/404'),
             changefreq: 'weekly',
